5f3845dd774ae7008d28160e0940b3bc70e7b386
apels1n <antonguryshkin@gmail.com>
2023-08-06 20:39:01 +0300

    Metadata track cover discovery added

--- resources/modules/metaviewer.py
+++ resources/modules/metaviewer.py
@@ -1,12 +1,13 @@
-from mutagen.easyid3 import EasyID3
+from mutagen.id3 import ID3, APIC
 from mutagen.mp3 import MP3
 
 class MetaViewer:
     def __init__(self, audio_file):
         self.__audio_file = audio_file
-        self.__audio = EasyID3(self.__audio_file)
-        self.__artist = self.__audio['artist'][0] or 'Unknown Artist'
-        self.__title = self.__audio['title'][0] or 'Unknown Title'
+        self.__audio = ID3(self.__audio_file)
+        self.__artist = self.__audio.get('TPE1').text[0] or 'Unknown Artist'
+        self.__title = self.__audio.get('TIT2').text[0] or 'Unknown Title'
+        self.__cover = self.__audio.getall('APIC')[0].data
 
     def getArtistAndTitle(self):
         return self.__artist, self.__title
@@ -18,3 +19,6 @@ class MetaViewer:
         seconds = duration_seconds % 60
 
         return minutes, seconds
+
+    def getTrackCover(self):
+        return self.__cover
